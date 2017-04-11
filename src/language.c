4d75465628a41f91973f64ba5bef30453d4e5347
= <=>
2017-04-11 19:33:49 +0700

    Utf-8 read char

--- src/language.c
+++ src/language.c
@@ -1,36 +1,58 @@
 #include <stdio.h>
 #include <string.h>
-
-int read_rus() 
+#include <wchar.h>
+#include <locale.h>
+int read_rus(FILE *in, wchar_t *input_word) 
 {
-    return 1;
-}
-
-int read_eng() 
-{
-    return 1;
-}
-
-int main()
-{
-
-	FILE *in = fopen("rus-eng.txt", "rt");
-    char il;
-
-
-    if(!in){
-        perror("error");
-        return -1;
-    }
+    wchar_t word;
     while(1) {
-        fscanf(in,"%c", &il);
-        printf("%zu ", il);
-        if(il == '\n') {
+        fscanf(in,"%C", &word);
+        if(word == '|') {
+            break;
+        } else {
+
+        }
+        printf("%C", word);
+    }
+    return 1;
+}
+
+int read_eng(FILE *in, wchar_t *input_word) 
+{
+    wchar_t word;
+    while(1) {
+        fscanf(in,"%C", &word);
+        if(word == '|') {
             break;
         } else {
 
         }
     }
+    while(1) {
+        fscanf(in,"%C", &word);
+        if(word == '\n') {
+            break;
+        } else {
+
+        }
+        printf("%C", word);
+    }
+    return 1;
+}
+
+int main()
+{   
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+	FILE *in = fopen("rus-eng.txt", "r");
+    if(!in){
+        perror("error");
+        return -1;
+    }
+    read_rus(in, L"р");
+    printf("\n");
+    read_eng(in, L"р");
+    printf("\n");
     fclose(in);
     return 0;
 }
